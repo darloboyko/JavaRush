62d7c6919540d7fb4173133038f2cc70cf02201a
darloboyko <darloboyko@gmail.com>
2018-06-24 15:16:36 +0300

    Initial commit

---
+++ Lavel0/Solution_1.java
@@ -0,0 +1,9 @@
+package Lavel0;
+
+public class Solution_1 {
+    public static void main(String[] args)
+    {
+        System.out.println("Амиго очень умный");
+        //напишите тут ваш код
+    }
+}
---
+++ Lavel0/Solution_2.java
@@ -0,0 +1,8 @@
+package Lavel0;
+
+public class Solution_2 {
+    public static void main(String[] args) {
+        System.out.print("Я думаю, быть программистом - это круто");
+        //напишите тут ваш код
+    }
+}
