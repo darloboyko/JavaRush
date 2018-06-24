adcee3e00b4d16db91d6e7b2807cd03227d31b5c
darloboyko <darloboyko@gmail.com>
2018-06-24 15:29:41 +0300

    add

---
+++ Lavel0/Solution_5.java
@@ -0,0 +1,11 @@
+package Lavel0;
+
+public class Solution_5 {
+    public static void main(String[] args)
+    {
+        String name;
+        int age;
+        String city;
+        //напишите тут ваш код
+    }
+}
