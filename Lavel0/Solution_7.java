82029cb7cf6691e87c3a64e02954f8d86f0da5cc
darloboyko <darloboyko@gmail.com>
2018-06-24 15:34:00 +0300

    Баги и фичи Напиши программу, которая выводит на экран надпись "Это не баг, это фича."
    Пример вывода на экран:
    Это не баг, это фича.
    
    Требования:
    1. Программа должна выводить текст.
    2. Текст должен начинаться с "Это не баг".
    3. Текст должен заканчиваться точкой.
    4. Текст должен состоять из 21 символа, включая пробелы и знаки препинания.
    5. Выводимый текст должен соответствовать заданию.

---
+++ Lavel0/Solution_7.java
@@ -0,0 +1,8 @@
+package Lavel0;
+
+public class Solution_7 {
+    public static void main(String[] args)
+    {
+        System.out.println("Это не баг, это фича."); //напишите тут ваш код
+    }
+}
