6214c8a5786fa47f48f9ca2c740e2ded92371331
darloboyko <darloboyko@gmail.com>
2018-06-24 15:49:16 +0300

    «Я по объявлению…» Объяви переменные a, b типа int. Сразу же в строке объявления присвой им разные значения. Значениями могут быть любые целые числа.
    Требования:
    1. Объяви две переменные типа int.
    2. Имена переменных должны быть a и b.
    3. Переменным сразу должны быть присвоены значения.
    4. Переменные a и b должны иметь разные значения.
    
    package com.javarush.task.task01.task0109;

---
+++ Lavel0/Solution_10.java
@@ -0,0 +1,13 @@
+package Lavel0;
+
+/*
+«Я по объявлению…»
+*/
+
+public class Solution_10 {
+    public static void main(String[] args)
+    { int a=4;
+        int b=8;
+        //напишите тут ваш код
+    }
+}
