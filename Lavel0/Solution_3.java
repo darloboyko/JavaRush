3d8217627fcba1d48214d44d725559714e070672
darloboyko <darloboyko@gmail.com>
2018-06-24 15:18:57 +0300

    https://javarush.ru/tasks/com.javarush.task.task01.task0142

---
+++ Lavel0/Solution_3.java
@@ -0,0 +1,14 @@
+package Lavel0;
+
+public class Solution_3 {
+    public static void main(String[] args)
+    {
+        String a="Элли самая красивая";
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        //напишите тут ваш код
+    }
+}
