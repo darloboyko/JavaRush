55b4ff7e08f04c727c06a295fd1ba779b60e25b6
darloboyko <darloboyko@gmail.com>
2018-06-24 15:24:14 +0300

    https://javarush.ru/tasks/com.javarush.task.task01.task0142

---
+++ Lavel0/Solution_4.java
@@ -0,0 +1,10 @@
+package Lavel0;
+
+public class Solution_4 {
+    public static void main(String[] args)
+    {
+        int d=3126-8;
+        System.out.println(d);
+        //напишите тут ваш код
+    }
+}
