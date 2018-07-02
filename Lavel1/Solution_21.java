1c66549574d6fc9e0e83e4642c97ce30c719a7aa
darloboyko <darloboyko@gmail.com>
2018-07-02 18:53:33 +0300

    Лучше поздно, чем никогда: Hello world! Каждый программист, когда начинает изучать какой-либо язык программирования, первой задачей пишет задачу "Hello World!". - Амиго, ты же хочешь стать программистом? - Конечно! - Тогда быстро пиши программу, которая выведет на экран фразу "Hello World!"
    Требования:
    1. Программа должна выводить текст.
    2. Текст должен начинаться с "Hello".
    3. Текст должен заканчиваться знаком восклицания.
    4. Текст должен состоять из 12 символов, включая пробелы и знак восклицания.
    5. Выводимый текст должен соответствовать заданию.

---
+++ Lavel1/Solution_21.java
@@ -0,0 +1,12 @@
+package Lavel1;
+
+/*
+Лучше поздно, чем никогда: Hello world!
+*/
+
+public class Solution_21 {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");//напишите тут ваш код
+    }
+}
