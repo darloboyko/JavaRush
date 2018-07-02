d1dc5cd777a4026f948b6b5356e9d1ecfb23eb4f
darloboyko <darloboyko@gmail.com>
2018-07-02 18:41:30 +0300

    Ну это… С новым годом! Убери комментарии с части кода, чтобы на экран вывелось сообщение "Happy New Year".
    Требования:
    1. Программа должна выводить на экран текст.
    2. Нужно раскомментировать некоторые строки. Больше ничего не менять.
    3. В программу нельзя добавлять новые строки или удалять имеющиеся.
    4. Выведенный текст должен соответствовать заданию.

---
+++ Lavel1/Solution_16.java
@@ -0,0 +1,20 @@
+package Lavel1;
+
+/*
+Ну это… С новым годом!
+*/
+
+public class Solution_16 {
+    public static void main(String[] args) {
+//        String s = "Happy";
+//        System.out.println("New Year");
+        //       System.out.println("Happy New");
+//        System.out.println("Year");
+        System.out.print("Happy New");
+//      System.out.println("Year");
+//        System.out.println(s);
+//        System.out.print("Happy New!");
+        System.out.print(" ");
+        System.out.println("Year");
+    }
+}
