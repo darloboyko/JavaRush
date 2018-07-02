10e63467e4a1647e73e2f1e231cb4ac3c49ff173
darloboyko <darloboyko@gmail.com>
2018-07-02 18:43:37 +0300

    Вносим изменения в код Внеси изменения в программу, чтобы переменная name имела значение Amigo.
    Требования:
    1. Переменной name должно присваиваться значение сразу после создания.
    2. Должно быть объявлено 2 переменных.
    3. Значение переменной text изменять нельзя.
    4. Команду вывода текста на экран изменять нельзя.
    5. Программа должна выводить текст "Hello Amigo!".

---
+++ Lavel1/Solution_17.java
@@ -0,0 +1,13 @@
+package Lavel1;
+
+/*
+Вносим изменения в код
+*/
+
+public class Solution_17 {
+    public static void main(String[] args) {
+        String name = "Amigo";
+        String text = "Hello " + name + "!";
+        System.out.println(text);
+    }
+}
