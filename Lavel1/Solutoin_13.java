f5dfd6d7003e775057fc30ced64c60f28a254b08
darloboyko <darloboyko@gmail.com>
2018-07-02 18:29:48 +0300

    Выводим квадрат числа Дана переменная number. Напиши программу, которая выводит на экран квадрат этой переменной (number * number).
    Требования:
    1. Переменную number не изменяй, можешь менять только ее значение.
    2. Программа должна выводить текст.
    3. Выводимый текст должен быть числом.
    4. Выводимый текст должен быть квадратом переменной number.

---
+++ Lavel1/Solutoin_13.java
@@ -0,0 +1,16 @@
+package Lavel1;
+
+
+/*
+Выводим квадрат числа
+*/
+
+public class Solutoin_13 {
+    public static int number = 456;
+
+    public static void main(String[] args)
+    {
+        int f=number*number;
+        System.out.println(f); //напишите тут ваш код
+    }
+}
