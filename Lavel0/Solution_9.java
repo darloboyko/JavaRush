bc8b7a5a44a4c4ef8422de8918a7debd92db561b
darloboyko <darloboyko@gmail.com>
2018-07-02 18:15:51 +0300

    Комментарии излишни Раскомментируй некоторые строчки, чтобы программа вывела на экран числа 12 и 2 (сначала 12, а затем 2).
    Требования:
    1. Программа должна выводить на экран числа 12 и 2.
    2. Нельзя изменять строки с объявлением переменных.
    3. Программа должна содержать только две переменные типа int.
    4. Нельзя изменять строки отвечающие за вывод в консоль.
    5. Нужно раскомментировать необходимые строки и не менять остальные.

--- Lavel0/Solution_9.java
+++ Lavel0/Solution_9.java
@@ -1,27 +1,20 @@
 package Lavel0;
 
 /*
-2 + 3 = 5
+Комментарии излишни
 */
 
 public class Solution_9 {
     public static void main(String[] args) {
-        String s = "23";
-        int a = 3, b = 2;
-        String four = "четыре";
-        String three = "3";
+        int x = 2;
+        int y = 12;
 
-        System.out.print(b);
-        //  System.out.print("два");//
-        System.out.print(" плюс ");
-        //  System.out.print(s); //
-        //  System.out.print(" плюс ");//
-        // System.out.print("три");//
-        System.out.print(a);
+        // x = x * 3;
+        y = x + y;
+        x = y - x;
+        y = y - x;
 
-        System.out.print(" равно ");
-        System.out.print(" равно ");
-        // System.out.print("пять"); //
-        System.out.print(a + b);
+        System.out.println(x);
+        System.out.println(y);
     }
 }
