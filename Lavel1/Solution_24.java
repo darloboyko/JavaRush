46dc49661764a337a1a80627dbe89259a0041e91
darloboyko <darloboyko@gmail.com>
2018-07-02 19:11:54 +0300

    Поиск ошибок Исправь ошибку в программе. Нужно чтобы программа выводила на экран число 100.
    Требования:
    1. Программа должна выводить на экран текст.
    2. Должно быть объявлено 3 переменные.
    3. Тип переменных менять нельзя.
    4. Значения переменных менять нельзя.
    5. В программу нельзя добавлять новые строки или удалять имеющиеся.
    6. Внеси минимальные изменения в команду вывода.
    7. Выведенный текст должен соответствовать заданию.

---
+++ Lavel1/Solution_24.java
@@ -0,0 +1,14 @@
+package Lavel1;
+
+/*
+Поиск ошибок
+*/
+
+public class Solution_24 {
+    public static void main(String[] args) {
+        int a = 56;
+        int b = 30;
+        int c = 14;
+        System.out.print(a + b + c);
+    }
+}
