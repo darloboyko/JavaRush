667bf32a5be5242f1121573cd1489f3a53d8e9b3
darloboyko <darloboyko@gmail.com>
2018-07-02 18:39:59 +0300

    JavaRush. Learn once - use anywhere Напиши программу, которая выводит на экран надпись: "JavaRush. Learn once - use anywhere" 10 раз.
    Требования:
    1. Программа должна выводить текст.
    2. Текст должен начинаться с "JavaRush".
    3. Текст должен заканчиваться на "use anywhere".
    4. Текст должен состоять из 10 строк.
    5. Выводимый текст должен соответствовать заданию.

---
+++ Lavel1/Solution_15.java
@@ -0,0 +1,23 @@
+package Lavel1;
+
+/*
+JavaRush. Learn once - use anywhere
+*/
+
+
+public class Solution_15 {
+    public static void main(String[] args)
+    {
+        System.out.println("JavaRush. Learn once - use anywhere");
+        System.out.println("JavaRush. Learn once - use anywhere");
+        System.out.println("JavaRush. Learn once - use anywhere");
+        System.out.println("JavaRush. Learn once - use anywhere");
+        System.out.println("JavaRush. Learn once - use anywhere");
+        System.out.println("JavaRush. Learn once - use anywhere");
+        System.out.println("JavaRush. Learn once - use anywhere");
+        System.out.println("JavaRush. Learn once - use anywhere");
+        System.out.println("JavaRush. Learn once - use anywhere");
+        System.out.println("JavaRush. Learn once - use anywhere");
+        //напишите тут ваш код
+    }
+}
