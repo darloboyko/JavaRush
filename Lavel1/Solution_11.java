75863ec8a7aba010274f0bf76944f4f6b496240d
darloboyko <darloboyko@gmail.com>
2018-07-02 18:21:35 +0300

    На нашем экране — переменная Объяви переменную name типа String. Сразу же в строке объявления присвой ей какое-нибудь значение. Выведи на экран переменную name.
    Требования:
    1. В методе main объяви переменную типа String.
    2. Имя переменной должно быть name.
    3. Переменной сразу должно быть присвоено значение.
    4. Программа должна выводить текст на экран.
    5. Выведенный текст должен быть значением переменной.

---
+++ Lavel1/Solution_11.java
@@ -0,0 +1,15 @@
+package Lavel1;
+
+
+/*
+На нашем экране — переменная
+*/
+
+public class Solution_11 {
+    public static void main(String[] args)
+    {
+        String name = "Mudak";
+        System.out.println(name);
+        //напишите тут ваш код
+    }
+}
