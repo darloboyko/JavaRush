1b555c65ae7e81b34e05a40b3e6d953fc78b30de
darloboyko <darloboyko@gmail.com>
2018-07-02 18:49:15 +0300

    Еще немного исправлений Внеси изменения в программу, чтобы выводился на экран текст: "Coding in java". Используй переменные.
    Требования:
    1. Должно быть объявлено 3 переменных.
    2. Всем переменным должны быть присвоены значение сразу после создания.
    3. Команду вывода текста на экран изменять нельзя.
    4. Программа должна выводить текст "Coding in java".

---
+++ Lavel1/Solution_18.java
@@ -0,0 +1,14 @@
+package Lavel1;
+
+/*
+Еще немного исправлений
+*/
+
+public class Solution_18 {
+    public static void main(String[] args) {
+        String s1 = "Coding";
+        String s2 = "in";
+        String s3 = "java";
+        System.out.println(s1 + " " + s2 + " " + s3);
+    }
+}
