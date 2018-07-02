029f78414d5b8d69f0bef4edccf8a76946f36bcc
darloboyko <darloboyko@gmail.com>
2018-07-02 18:23:38 +0300

    Одной переменной недостаточно Объяви переменные a, b типа int и переменную s типа String. Сразу же в строке объявления присвой им любые разные значения.
    Требования:
    1. Объяви две переменные типа int.
    2. Объяви одну переменную типа String.
    3. Имена переменных типа int должны быть a и b.
    4. Имя переменной типа String должно быть s.
    5. Переменным сразу должны быть присвоены значения.
    6. Переменные a и b должны иметь разные значения.

---
+++ Lavel1/Solution_12.java
@@ -0,0 +1,15 @@
+package Lavel1;
+
+/*
+Одной переменной недостаточно
+*/
+
+  public class Solution_12 {
+      public static void main(String[] args)
+         {
+         int a=5;
+         int b=7;
+         String s="blabla";
+         //напишите тут ваш код
+         }
+}
