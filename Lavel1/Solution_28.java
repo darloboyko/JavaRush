66158c2f1cb64b69fff6dfcac782d2e3fbb2b798
darloboyko <darloboyko@gmail.com>
2018-07-02 19:17:24 +0300

    Большая чистка Объяви недостающие переменные и закомментируй те, которые нигде не используются.
    Требования:
    1. Программа должна выводить на экран текст.
    2. Должно быть объявлено 4 переменные.
    3. Нужно закомментировать неиспользуемые переменные.
    4. Вывод программы не должен измениться.

---
+++ Lavel1/Solution_28.java
@@ -0,0 +1,18 @@
+package Lavel1;
+
+/*
+Большая чистка
+*/
+
+public class Solution_28 {
+    public static void main(String[] args) {
+        //    String s = "15";
+        int a = 5;
+        //    int z = 18;
+        int d = 18;
+        int c = a + d;
+        String b = " better than ";
+
+        System.out.println(a + b + c);
+    }
+}
