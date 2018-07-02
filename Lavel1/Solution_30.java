2fa522a0ce0bc81f9cbff1a5b7319e76d668cbaf
darloboyko <darloboyko@gmail.com>
2018-07-02 19:22:06 +0300

    Число в квадрате Напиши программу, которая выводит на экран квадрат числа 5.
    Требования:
    1. Программа должна выводить текст.
    2. Метод main должен вызывать метод sqr с параметром 5.
    3. Метод main должен выводить результат метода sqr.
    4. Метод sqr изменять нельзя.
    5. Выводимый текст должен соответствовать заданию.

---
+++ Lavel1/Solution_30.java
@@ -0,0 +1,17 @@
+package Lavel1;
+
+/*
+Число в квадрате
+*/
+
+public class Solution_30 {
+    public static void main(String[] args) {
+        int a = 5;
+        int sqr = sqr(a); //напишите тут ваш код
+    }
+
+    public static int sqr(int a) {
+        return a * a;
+    }
+}
+
