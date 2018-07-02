1079c54a1097ef56895876efd1840002912fbb32
darloboyko <darloboyko@gmail.com>
2018-07-02 18:50:40 +0300

    Закомментируйте лишнее Закомментируй часть кода, чтобы на экран вывелось сообщение "summa = 12"
    Требования:
    1. Программа должна выводить на экран текст.
    2. Нужно закомментировать необходимые строки и не менять остальные.
    3. В программу нельзя добавлять новые строки или удалять имеющиеся.
    4. Выведенный текст должен соответствовать заданию.

---
+++ Lavel1/Solution_19.java
@@ -0,0 +1,20 @@
+package Lavel1;
+
+/*
+Закомментируйте лишнее
+*/
+
+public class Solution_19 {
+    public static void main(String[] args) {
+        int a = 3;
+        //     int a = 10;
+        int b = 6;
+        //     int b = 12;
+        //     int summa = 1 + a + b;
+        //     int summa = 2 + a + b;
+        int summa = 3 + a + b;
+        //     int summa = 4 + a + b;
+
+        System.out.println("summa = " + summa);
+    }
+}
