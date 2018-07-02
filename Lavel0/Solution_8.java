940f7366cf4dcaaddc3be77d032198c114ceb11e
darloboyko <darloboyko@gmail.com>
2018-07-02 18:14:24 +0300

    2 + 3 = 5 Закомментируй несколько строк, чтобы на экран вывелась надпись: 2 плюс 3 равно 5 Примечание: комментировать строки с объявлением переменных нельзя.
    Требования:
    1. Программа должна выводить на экран надпись "2 плюс 3 равно 5"
    2. Нельзя изменять строки с объявлением переменных.
    3. Нужно закомментировать минимум одну строку.
    4. Нельзя изменять (можно закомментировать) или добавлять команды, отвечающие за вывод.

--- Lavel0/Solution_8.java
+++ Lavel0/Solution_8.java
@@ -2,15 +2,20 @@ package Lavel0;
 
 public class Solution_8 {
     public static void main(String[] args) {
-        int x = 2;
-        int y = 12;
+        String s = "23";
+        int a = 3, b = 2;
+        String four = "четыре";
+        String three = "3";
 
-        // x = x * 3;
-        y = x + y;
-        x = y - x;
-        y = y - x;
-
-        System.out.println(x);
-        System.out.println(y);
+        System.out.print(b);
+        //  System.out.print("два");//
+        System.out.print(" плюс ");
+        //  System.out.print(s); //
+        //  System.out.print(" плюс ");//
+        // System.out.print("три");//
+        System.out.print(a);
+        System.out.print(" равно ");
+        // System.out.print("пять"); //
+        System.out.print(a + b);
     }
 }
