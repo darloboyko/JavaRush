bafdda1ecfcf7e633a2b095602622ea5c00b3592
darloboyko <darloboyko@gmail.com>
2018-07-02 19:26:00 +0300

    Просто, как 2+2 Напиши программу, которая вызывает метод sum с параметрами 2 и 2.
    Требования:
    1. Программа должна выводить сумму 2 и 2.
    2. Метод main должен вызывать метод sum с параметрами 2 и 2.
    3. Метод main не должен вызывать команду вывода текста на экран.
    4. Метод sum изменять нельзя.

---
+++ Lavel1/Solution_31.java
@@ -0,0 +1,17 @@
+package Lavel1;
+
+/*
+Просто, как 2+2
+*/
+
+public class Solution_31 {
+    public static void main(String[] args) {
+        sum(2,2); //напишите тут ваш код
+    }
+
+    public static void sum(int a, int b) {
+        int c = a + b;
+        System.out.print(c);
+    }
+
+}
