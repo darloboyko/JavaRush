7f2e67012725308af0ca271dc795c45618ddf0ce
darloboyko <darloboyko@gmail.com>
2018-07-02 20:50:34 +0300

    Реализуем метод print В методе print выведи на экран переданную строку 4 раза. Каждый раз с новой строки.
    Требования:
    1. Программа должна выводить текст на экран.
    2. Метод main не должен вызывать System.out.println или System.out.print.
    3. Метод print должен выводить текст на экран.
    4. Метод main должен вызвать метод print класса Solution ровно два раза.
    5. Метод print должен выводить на экран строку 4 раза. Каждый раз с новой строки.

---
+++ Lavel2/Solution_33.java
@@ -0,0 +1,21 @@
+package Lavel2;
+
+/*
+Реализуем метод print
+*/
+
+public class Solution_33 {
+
+    public static void main(String[] args) {
+        print("Java easy to learn!");
+        print("Java opens many opportunities!");
+    }
+
+    public static void print(String s) {
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);//напишите тут ваш код
+    }
+}
+
