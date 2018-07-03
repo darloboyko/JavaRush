c8c7fff5a665ee12102e1bd9b82c8c161d62bcc6
darloboyko <darloboyko@gmail.com>
2018-07-03 19:43:07 +0300

    Считаем длину окружности Выведи на экран длину окружности, рассчитанную по формуле: L = 2 * Pi * radius. Результат - дробное число (тип double). В качестве значения Pi используй значение 3.14.
    Требования:
    1. В методе printCircleLength нужно вывести длину окружности, рассчитанную по формуле: 2 * Pi * radius.
    2. Метод main должен вызывать метод printCircleLength с параметром 5.
    3. Метод main не должен вызывать команду вывода текста на экран.
    4. Программа должна выводить длину окружности с радиусом 5.

---
+++ Lavel2/Solution_35.java
@@ -0,0 +1,17 @@
+package Lavel2;
+
+/*
+Считаем длину окружности
+*/
+
+public class Solution_35 {
+    public static void main(String[] args) {
+        printCircleLength(5);
+
+    }
+
+    public static void printCircleLength(int radius) {
+        double Pi = 3.14;
+        System.out.print(2*Pi*radius);//напишите тут ваш код
+    }
+}
