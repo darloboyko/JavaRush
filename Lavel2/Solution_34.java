cea6aa09ff3658efd1418e1bbc9c903fdf2c7673
darloboyko <darloboyko@gmail.com>
2018-07-03 19:50:21 +0300

    Прибавка к зарплате В методе public static void salary(int a) увеличь зарплату на 100 и выведи на экран надпись: "Твоя зарплата составляет: a долларов в месяц." Где a - это зарплата, которую увеличили на 100.
    Пример вывода на экран для а = 700:
    Твоя зарплата составляет: 800 долларов в месяц.
    
    Требования:
    1. Программа должна выводить текст на экран.
    2. Метод main не должен вызывать System.out.println или System.out.print.
    3. Метод salary не должен возвращать результат.
    4. Метод main должен вызвать метод salary только один раз.
    5. Метод salary должен увеличить переданное число на 100 и вывести на экран надпись согласно шаблону.

---
+++ Lavel2/Solution_34.java
@@ -0,0 +1,17 @@
+package Lavel2;
+
+/*
+Прибавка к зарплате
+*/
+
+public class Solution_34 {
+    public static void main(String[] args) {
+        salary(700);
+    }
+
+    public static void salary(int a) {
+int i = 100;
+int b = a + i;
+        System.out.print("Твоя зарплата составляет: " + b + " долларов в месяц.");//напишите тут ваш код
+    }
+}
