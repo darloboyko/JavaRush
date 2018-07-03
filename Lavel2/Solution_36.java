b9dc5c3b598f4b5cef7277b1e652a775a600494b
darloboyko <darloboyko@gmail.com>
2018-07-03 19:59:49 +0300

    Кусочек калькулятора В методе main объяви две переменные a и b типа int. Присвой им любые значения. Выведи на экран их сумму и произведение. Каждое значение выводить с новой строки.
    Пример вывода на экран для а = 5, b = 7:
    12
    35
    
    Требования:
    1. Программа должна выводить текст на экран.
    2. В программе должны быть только две переменные a и b типа int.
    3. Переменным сразу должны быть присвоены значения.
    4. Метод main должен выводить на экран сумму переменных a и b.
    5. Метод main должен выводить на экран произведение переменных a и b.
    6. Каждое значение должно быть выведено с новой строки

---
+++ Lavel2/Solution_36.java
@@ -0,0 +1,14 @@
+package Lavel2;
+
+/*
+Кусочек калькулятора
+*/
+
+public class Solution_36 {
+    public static void main(String[] args) {
+        int a = 5, b = 8;
+        System.out.println(a+b);
+        System.out.println(a*b);
+        //напишите тут ваш код
+    }
+}
