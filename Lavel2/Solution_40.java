14d894966fa31e67ead77fa788115f3557af22df
darloboyko <darloboyko@gmail.com>
2018-07-13 20:00:48 +0300

    Одного кота нам мало Создать объект типа Cat 2 раза. Сохрани каждый экземпляр в свою переменную. Имена переменных должны быть разные.
    Требования:
    1. Программа не должна выводить текст на экран.
    2. В методе main должно быть только две переменные типа Cat.
    3. Переменным сразу должны быть присвоены значения.
    4. В классе Cat не должно быть переменных.
    5. В классе Cat не должно быть методов.

---
+++ Lavel2/Solution_40.java
@@ -0,0 +1,13 @@
+package Lavel2;
+
+public class Solution_40 {
+    public static void main(String[] args) {
+        Cat barsik = new Cat();
+        Cat muska = new Cat();
+        //напишите тут ваш код
+    }
+
+    public static class Cat {
+
+    }
+}
