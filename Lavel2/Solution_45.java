bdbe1de60ebe24b331f306e753bd165477c85a18
darloboyko <darloboyko@gmail.com>
2018-07-15 09:45:54 +0300

    Питомцам нужны люди Создай объект типа Cat (кот), объект типа Dog (собака), объект типа Fish (рыбка) и объект типа Woman. Присвой каждому животному владельца (owner).
    Требования:
    1. Программа не должна выводить текст на экран.
    2. В методе main создай объекты типа Cat, Dog, Fish, Woman занеси их ссылки в переменные.
    3. В методе main присвойте каждому животному владельца Woman.
    4. Класс Cat, Dog, Fish должен содержать только одну переменную Woman owner.
    5. Класс Woman не должен содержать переменных.

---
+++ Lavel2/Solution_45.java
@@ -0,0 +1,28 @@
+package Lavel2;
+
+public class Solution_45 {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        Dog dog = new Dog();
+        Fish fish = new Fish();
+        Woman woman = new Woman();
+        cat.owner = woman;
+        dog.owner = woman;
+        fish.owner = woman;//напишите тут ваш код
+    }
+
+    public static class Cat {
+        public Woman owner;
+    }
+
+    public static class Dog {
+        public Woman owner;
+    }
+
+    public static class Fish {
+        public Woman owner;
+    }
+
+    public static class Woman {
+    }
+}
