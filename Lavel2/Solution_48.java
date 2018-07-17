f0151f6fe17bd1d04feca790fdeb9e0f4258d1bf
darloboyko <darloboyko@gmail.com>
2018-07-17 18:35:52 +0300

    Печатаем трижды Реализуй метод print3. Метод должен вывести переданную строку (слово) на экран три раза через пробел.
    Требования:
    1. Программа должна выводить текст на экран.
    2. Метод main не должен вызывать функцию System.out.println или System.out.print.
    3. Метод print3 должен выводить текст на экран.
    4. Метод main должен вызвать метод print3 ровно два раза.
    5. Метод print3 должен выводить переданную строку (слово) на экран три раза, но в одной строке.

---
+++ Lavel2/Solution_48.java
@@ -0,0 +1,15 @@
+package Lavel2;
+
+public class Solution_48 {
+    public static void print3(String s) {
+        System.out.print(s + " ");
+        System.out.print(s + " ");
+        System.out.println(s);//напишите тут ваш код
+
+    }
+
+    public static void main(String[] args) {
+        print3("window");
+        print3("file");
+    }
+}
