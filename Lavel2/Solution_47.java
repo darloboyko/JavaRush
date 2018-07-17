54323fb030f061da2bb8649174781c859f1bcbce
darloboyko <darloboyko@gmail.com>
2018-07-17 18:22:45 +0300

    Повторенье-мать Реализуй метод print3. Метод должен вывести на экран переданную строку 3 раза. Каждый раз с новой строки.
    Требования:
    1. Программа должна выводить текст на экран.
    2. Метод main не должен вызывать функцию System.out.println или System.out.print.
    3. Метод print3 должен выводить текст на экран.
    4. Метод main должен вызвать метод print3 только один раз.
    5. Метод print3 должен выводить на экран строку 3 раза. Каждый раз с новой строки.

---
+++ Lavel2/Solution_47.java
@@ -0,0 +1,15 @@
+package Lavel2;
+
+public class Solution_47 {
+    public static void print3(String s) {
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        //напишите тут ваш код
+
+    }
+
+    public static void main(String[] args) {
+        print3("I love you!");
+    }
+}
