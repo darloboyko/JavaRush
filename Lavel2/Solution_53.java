f984bfa9e9999c11d08bd1bc66da0e4b394de2c9
darloboyko <darloboyko@gmail.com>
2018-07-30 16:48:08 +0300

    Обмен валют Напиши код метода convertEurToUsd, который переводит евро в доллары по заданному курсу. Для возврата результата из метода convertEurToUsd используй оператор return. Пример: return 123*435; Вызови метод convertEurToUsd дважды в методе main с любыми параметрами. Результаты выведи на экран, каждый раз с новой строки.
    Подсказка:
    Расчет выполняется по формуле: долларСША = евро * курс
    
    Требования:
    1. Метод convertEurToUsd должен умножать евро на курс и возвращать полученный результат.
    2. Метод main должен 2 раза вызвать метод convertEurToUsd с любыми параметрами.
    3. Метод main должен выводить результаты вызовов на экран, каждый раз с новой строки.
    4. Метод convertEurToUsd не должен ничего выводить на экран.

--- Lavel2/Solution_53.java
+++ Lavel2/Solution_53.java
@@ -1,12 +1,19 @@
 package Lavel2;
 
 public class Solution_53 {
-    public static void printString(String s);
-    System.out.println();
+
 
     //напишите тут ваш код
 
-    public static void main(String[] args) {
+
+    public static void printString(String s)
+    {
+        System.out.println(s);
+        //напишите тут ваш код
+    }
+
+    public static void main(String[] args)
+    {
         printString("Hello Amigo!");
     }
 }
