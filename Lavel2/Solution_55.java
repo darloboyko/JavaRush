7d9b40b6a8817aab73baa1fdecc8a75b644d98a3
darloboyko <darloboyko@gmail.com>
2018-07-30 16:56:06 +0300

    Задача на проценты Напиши код метода addTenPercent, который увеличивает переданное целое число на 10%. Для возврата результата из метода addTenPercent используй оператор return.
    Пример:
    return 123 * 435;
    
    Требования:
    1. Метод addTenPercent должен увеличивать переданное число на 10% процентов.
    2. Метод main должен вызывать метод addTenPercent.
    3. Метод main должен выводить результаты вызова на экран.
    4. Метод addTenPercent не должен ничего выводить на экран.

---
+++ Lavel2/Solution_55.java
@@ -0,0 +1,11 @@
+package Lavel2;
+
+public class Solution_55 {
+    public static double addTenPercent(int i) {
+        return i+i*0.1; //напишите тут ваш код
+    }
+
+    public static void main(String[] args) {
+        System.out.println(addTenPercent(9));
+    }
+}
