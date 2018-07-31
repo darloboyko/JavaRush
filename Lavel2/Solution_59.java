2bafaeaa531e4642f3e527a38f501948f1b1afe8
darloboyko <darloboyko@gmail.com>
2018-07-31 11:21:09 +0300

    Произведение 10 чисел Вывести на экран произведение 10 чисел от 1 до 10. Результат - это 1 число.
    Подсказка:
    будет три миллиона с хвостиком.
    
    Требования:
    1. Программа должна выводить целое число на экран.
    2. Метод main должен вызывать функцию System.out.println.
    3. Выведенное число должно быть больше трех миллионов.
    4. Выведенное число должно соответствовать заданию.

---
+++ Lavel2/Solution_59.java
@@ -0,0 +1,9 @@
+package Lavel2;
+
+public class Solution_59 {
+    public static void main(String[] args) {
+        int i = 1*2*3*4*5*6*7*8*9*10;
+        System.out.println(i); //напишите тут ваш код
+    }
+    
+}
