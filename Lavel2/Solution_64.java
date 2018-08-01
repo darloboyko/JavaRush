abf0ae2ab195308249801f11c502b5a5ee24d4df
darloboyko <darloboyko@gmail.com>
2018-08-01 17:35:21 +0300

    Мама мыла раму Вывести на экран все возможные комбинации слов "Мама", "Мыла", "Раму".
    Подсказка: их 6 штук.
    Каждую комбинацию вывести с новой строки. Слова не разделять.
    
    Пример:
    МылаРамуМама
    РамуМамаМыла
    ...
    
    Требования:
    1. Программа должна выводить текст.
    2. Выведенный текст должен содержать 6 строк.
    3. Текст в каждой строке должен быть уникален.
    4. Должны быть выведены все возможные комбинации.

---
+++ Lavel2/Solution_64.java
@@ -0,0 +1,13 @@
+package Lavel2;
+
+public class Solution_64 {
+    public static void main(String[] args) {
+        System.out.println("МамаМылаРаму");
+        System.out.println("РамуМамаМыла");
+        System.out.println("МамаРамуМыла");
+        System.out.println("МылаРамуМама");
+        System.out.println("МылаМамаРаму");
+        System.out.println("РамуМылаМама");
+        //напишите тут ваш код
+    }
+}
