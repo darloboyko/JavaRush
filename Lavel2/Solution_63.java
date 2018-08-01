0f065cd53885e57723588ced8d90d1f1b815cf23
darloboyko <darloboyko@gmail.com>
2018-08-01 17:20:00 +0300

    Конвертируем время Добавь метод public static int convertToSeconds(int hour) который будет конвертировать часы в секунды. Вызови его дважды в методе main с любыми параметрами. Результаты выведи на экран, каждый раз с новой строки.
    Требования:
    1. Добавь метод convertToSeconds, он должен быть public static, возвращать и принимать int.
    2. Метод convertToSeconds должен переводить переданные часы в секунды.
    3. Метод main должен дважды вызывать метод convertToSeconds.
    4. Метод main должен выводить результаты вызова на экран, каждый раз с новой строки.
    5. Метод convertToSeconds не должен ничего выводить на экран.

---
+++ Lavel2/Solution_63.java
@@ -0,0 +1,14 @@
+package Lavel2;
+
+public class Solution_63 {
+
+        public static int convertToSeconds(int hour) {
+            return (hour*3600);
+            //напишите тут ваш код
+        }
+        public static void main(String[] args) {
+            System.out.println(convertToSeconds(12));
+            System.out.println(convertToSeconds(17));
+            //напишите тут ваш код
+        }
+}
