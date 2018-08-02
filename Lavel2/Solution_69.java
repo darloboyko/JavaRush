628ca2d750f8edc3c5b4922ab653b0b98cf709be
darloboyko <darloboyko@gmail.com>
2018-08-02 23:44:43 +0300

    План по захвату мира Ввести с клавиатуры число и имя, вывести на экран строку: "имя" захватит мир через "число" лет. Му-ха-ха!
    Пример:
    Вася захватит мир через 8 лет. Му-ха-ха!
    
    Последовательность вводимых данных имеет большое значение.
    
    Требования:
    1. Программа должна выводить текст.
    2. Программа должна считывать данные с клавиатуры.
    3. Выведенный текст должен содержать введенное имя.
    4. Выведенный текст должен содержать введенное число.
    5. Выведенный текст должен полностью соответствовать заданию.

---
+++ Lavel2/Solution_69.java
@@ -0,0 +1,18 @@
+package Lavel2;
+
+
+import java.io.*;
+
+public class Solution_69 {
+
+    public static void main(String args[]) throws Exception {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader1 = new BufferedReader(new InputStreamReader(System.in));
+        String sAge = reader1.readLine();
+        String name = reader.readLine();
+        int nAge = Integer.parseInt(sAge);
+        System.out.println(name + " захватит мир через " + sAge + " лет. Му-ха-ха!");
+        //напишите тут ваш код
+    }
+}
