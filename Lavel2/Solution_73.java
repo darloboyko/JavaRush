ca6fb8909fd7e76a4ad8daada57d364843346f1b
darloboyko <darloboyko@gmail.com>
2018-08-03 00:14:55 +0300

    Полнометражная картина Реализуй метод getMetreFromCentimetre(int centimetre). Метод на вход принимает количество сантиметров. Нужно сделать так, чтобы метод возвращал число полных метров в centimetre. (1 метр = 100 см).
    Пример:
    Метод getMetreFromCentimetre вызывается с параметром 243.
    
    Пример вывода:
    2
    
    Требования:
    1. Метод getMetreFromCentimetre(int) должен быть публичным и статическим.
    2. Метод getMetreFromCentimetre должен возвращать значение типа int.
    3. Метод getMetreFromCentimetre не должен ничего выводить на экран.
    4. Метод getMetreFromCentimetre должен правильно возвращать количество полных метров в centimetre.

---
+++ Lavel2/Solution_73.java
@@ -0,0 +1,12 @@
+package Lavel2;
+
+public class Solution_73 {
+    public static void main(String[] args) {
+        System.out.println(getMetreFromCentimetre(243));
+    }
+
+    public static int getMetreFromCentimetre(int centimetre) {
+        int a;
+        return a = centimetre/100; //напишите тут ваш код
+    }
+}
