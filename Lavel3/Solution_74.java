4574ed7670b5f3c1665d74bc5d19770bb9c30ac9
darloboyko <darloboyko@gmail.com>
2018-08-03 00:25:58 +0300

    Большая и чистая Ввести с клавиатуры три имени, вывести на экран надпись: name1 + name2 + name3 = Чистая любовь, да-да!
    Пример:
    Вася + Ева + Анжелика = Чистая любовь, да-да!
    
    Требования:
    1. Программа должна выводить текст.
    2. Программа должна считывать данные с клавиатуры.
    3. Выведенный текст должен содержать введенное имя name1.
    4. Выведенный текст должен содержать введенное имя name2.
    5. Выведенный текст должен содержать введенное имя name3.
    6. Выведенный тест должен полностью соответствовать заданию.

---
+++ Lavel3/Solution_74.java
@@ -0,0 +1,25 @@
+package Lavel3;
+
+
+/*
+Большая и чистая
+*/
+
+
+import java.io.*;
+
+public class Solution_74 {
+    public static void main(String args[]) throws Exception {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader1 = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader2 = new BufferedReader(new InputStreamReader(System.in));
+        String name1 = reader.readLine();
+        String name2 = reader.readLine();
+        String name3 = reader.readLine();
+        System.out.println(name1 + " + " + name2 + " + " + name3 + " = " + "Чистая любовь, да-да!");
+        // Вася + Ева + Анжелика = Чистая любовь, да-да!
+    }
+
+
+}
