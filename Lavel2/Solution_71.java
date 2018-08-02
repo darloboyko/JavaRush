fec71c8e3ba0e24361ff746d2921fea160583777
darloboyko <darloboyko@gmail.com>
2018-08-02 23:58:29 +0300

    Скромность украшает программиста Ввести с клавиатуры имя и вывести надпись: name зарабатывает $5,000. Ха-ха-ха!
    Пример:
    Тимур зарабатывает $5,000. Ха-ха-ха!
    
    Требования:
    1. Программа должна выводить текст.
    2. Программа должна считывать данные с клавиатуры.
    3. Выведенный текст должен содержать введенное имя.
    4. Выведенный тест должен полностью соответствовать заданию.

---
+++ Lavel2/Solution_71.java
@@ -0,0 +1,18 @@
+package Lavel2;
+
+/*
+Скромность украшает программиста
+*/
+
+import java.io.*;
+
+public class Solution_71 {
+    public static void main(String args[]) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String name = reader.readLine();
+
+        System.out.println(name + " зарабатывает $5,000. Ха-ха-ха!");
+        // Тимур зарабатывает $5,000. Ха-ха-ха!
+    }
+}
