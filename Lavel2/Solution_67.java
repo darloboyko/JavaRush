d1fa4122d11029185ac90450dec02e9a6cbe4606
darloboyko <darloboyko@gmail.com>
2018-08-02 11:20:28 +0300

    Экранирование символов Вывести на экран следующий текст - две строки: It's Windows path: "C:\Program Files\Java\jdk1.7.0\bin" It's Java string: \"C:\\Program Files\\Java\\jdk1.7.0\\bin\"
    Подсказка:
    \” – экранирование двойной кавычки;
    \\ – экранирование обратной косой черты (\).
    
    Больше про экранирование символов и Escape-последовательности в Java читай в статье.
    
    Требования:
    1. Программа должна выводить текст.
    2. Должно быть выведено две строки.
    3. Текст первый строки должен быть: It's Windows path: "C:\Program Files\Java\jdk1.7.0\bin"
    4. Текст второй строки должен быть: It's Java string: \"C:\\Program Files\\Java\\jdk1.7.0\\bin\"

---
+++ Lavel2/Solution_67.java
@@ -0,0 +1,15 @@
+package Lavel2;
+
+/*
+Экранирование символов
+*/
+
+public class Solution_67 {
+    public static void main(String[] args) {
+        System.out.println("It's Windows path: \"C:\\Program Files\\Java\\jdk1.7.0\\bin\"");  //    It's Windows path: "C:\Program Files\Java\jdk1.7.0\bin"
+        System.out.println("It's Java string: \\\"C:\\\\Program Files\\\\Java\\\\jdk1.7.0\\\\bin\\\"");
+        //  It's Java string: \"C:\\Program Files\\Java\\jdk1.7.0\\bin\"
+        //напишите тут ваш код
+    }
+}
+
